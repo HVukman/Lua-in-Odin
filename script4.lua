73810200069ff3026dbd189de2fb8d2184503233
HVukman <peterslopp@googlemail.com>
2026-07-30 14:56:01 +0200

    Update script4.lua

--- script4.lua
+++ script4.lua
@@ -2,3 +2,14 @@
 function my_function(a, b)
     return a * b
 end
+
+function consume_table(table_)
+    return table_[1] + table_[2]
+end
+
+function consume_table2(table_)
+    -- return table
+    local result = {}
+    table.insert(result, table_[1]+ table_[2])
+    return result
+end
